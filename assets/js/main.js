a40e75eb7041d4972caf5ecd57bf268f6f4b08e1
Andrei Iurchenko <iur4.andrej@yandex.ru>
2023-09-11 21:22:12 +0300

    popular section done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,30 @@
 /*=============== CHANGE BACKGROUND HEADER ===============*/
-
+function scrollHeader() {
+  const header = document.getElementById("header");
+  if (this.scrollY >= 50) header.classList.add("scroll-header");
+}
+window.addEventListener("scroll", scrollHeader);
 
 /*=============== SWIPER POPULAR ===============*/
+var swiper = new Swiper(".popular__container", {
+  spaceBetween: 32,
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPreview: 'auto',
+  loop: true,
 
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
 
 /*=============== VALUE ACCORDION ===============*/
 
-
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
+/*=============== SHOW SCROLL UP ===============*/
 
-/*=============== SHOW SCROLL UP ===============*/ 
-
-
-/*=============== DARK LIGHT THEME ===============*/ 
-
+/*=============== DARK LIGHT THEME ===============*/
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
