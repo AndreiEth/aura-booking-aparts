cc8b3c8ca213c44018e962a23ff4b94200de017d
Andrei Iurchenko <iur4.andrej@yandex.ru>
2023-09-11 22:59:39 +0300

    accordion created

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,7 +10,7 @@ var swiper = new Swiper(".popular__container", {
   spaceBetween: 32,
   grabCursor: true,
   centeredSlides: true,
-  slidesPreview: 'auto',
+  slidesPreview: "auto",
   loop: true,
 
   navigation: {
@@ -20,6 +20,33 @@ var swiper = new Swiper(".popular__container", {
 });
 
 /*=============== VALUE ACCORDION ===============*/
+const accordionItems = document.querySelectorAll(".value__accordion-item");
+
+accordionItems.forEach((item) => {
+  const accordionHeader = item.querySelector(".value__accordion-header");
+
+  accordionHeader.addEventListener("click", () => {
+    const openItem = document.querySelector(".accordion-open");
+
+    toggleItem(item);
+
+    if (openItem && openItem !== item) {
+      toggleItem(openItem);
+    }
+  });
+});
+
+const toggleItem = (item) => {
+  const accordionContent = item.querySelector(".value__accordion-content");
+
+  if (item.classList.contains("accordion-open")) {
+    accordionContent.removeAttribute("style");
+    item.classList.remove("accordion-open");
+  } else {
+    accordionContent.style.height = accordionContent.scrollHeight + "px";
+    item.classList.add("accordion-open");
+  }
+};
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
